a428bdeea2394d4d59051dfcbc0b71b03605b0b0
lkmSasanga <lkmsasanga@gmail.com>
2020-12-18 18:31:54 +0530

    first commit

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class FormUserDetails extends Component{
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default FormUserDetails
